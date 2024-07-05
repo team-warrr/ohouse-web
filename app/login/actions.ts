1f512abc0bf95799771c1412b5ea62e49b3791c1
ShinYoung-Kim <lukey0515@naver.com>
2024-07-06 03:29:30 +0900

    feat: 로그인, 회원가입 action 생성

---
+++ app/login/actions.ts
@@ -0,0 +1,49 @@
+"use server";
+
+import { createClient } from "@/libs/supabase/server";
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
+
+export async function login(formData: FormData) {
+	const supabase = createClient();
+
+	// type-casting here for convenience
+	// in practice, you should validate your inputs
+	const data = {
+		email: formData.get("email") as string,
+		password: formData.get("password") as string,
+	};
+
+	const { error } = await supabase.auth.signInWithPassword(data);
+
+	if (error) {
+		redirect(encodeURI("/login?message=로그인 중 오류가 발생하였습니다"));
+	}
+
+	redirect("/user");
+}
+
+export async function signup(formData: FormData) {
+	const supabase = createClient();
+	const origin = headers().get("origin");
+
+	// type-casting here for convenience
+	// in practice, you should validate your inputs
+	const data = {
+		email: formData.get("email") as string,
+		password: formData.get("password") as string,
+	};
+
+	const { error } = await supabase.auth.signUp({
+		...data,
+		options: {
+			emailRedirectTo: `${origin}/auth/confirm`,
+		},
+	});
+
+	if (error) {
+		redirect(encodeURI("/login?message=회원 가입 중 오류가 발생하였습니다"));
+	}
+
+	redirect(encodeURI("/login?message=이메일을 확인해주세요."));
+}
