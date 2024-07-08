3368a251c6123ee739d719a6f6f798715ff2b3ef
ghdtjgus76 <ghdtjgus76@naver.com>
2024-07-08 15:27:34 +0900

    feat: shadcn ui 세팅

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
-import { Inter } from "next/font/google";
-
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,8 +19,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
+        {children}
+      </body>
     </html>
   );
 }
