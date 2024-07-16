815a32efc0f36f70c68f64f47aa7be3d5d3ccc62
ghdtjgus76 <ghdtjgus76@naver.com>
2024-07-16 20:15:37 +0900

    fix: 린트 에러 해결

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
+import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 
 const fontSans = FontSans({
