bfc68e8d5a5ca7c38c81f7cb6ff129bb51bafc15
ghdtjgus76 <ghdtjgus76@naver.com>
2024-07-28 18:09:06 +0900

    chore: root layout 디자인 수정

--- shared/components/view.tsx
+++ shared/components/view.tsx
@@ -1,7 +1,5 @@
 import { PropsWithChildren } from "react";
 
 export default function View(props: PropsWithChildren) {
-  return (
-    <main className="flex flex-col items-center justify-center gap-2 p-2">{props.children}</main>
-  );
+  return <main className="flex flex-col gap-2 p-2 max-w-[500px] mx-auto">{props.children}</main>;
 }
