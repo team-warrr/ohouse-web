e3cd5d62328e99d510a154e0825c8b35aa8688ec
ghdtjgus76 <ghdtjgus76@naver.com>
2024-07-28 14:53:10 +0900

    design: 행운 출첵 페이지 버튼 디자인 수정

--- app/attendance/page.tsx
+++ app/attendance/page.tsx
@@ -7,11 +7,11 @@ export default function AttendancePage() {
   return (
     <View>
       <Image alt="attendance" src="/attendance-image.avif" width={500} height={500} />
-      <Button fullWidth size="lg" color="primary">
+      <Button fullWidth size="lg" color="primary" className="rounded-[4px]">
         앱으로 입장하기 &gt;
       </Button>
       <Image alt="event-notice" src="/event-notice-image.avif" width={500} height={500} />
-      <Button fullWidth size="lg" color="primary">
+      <Button fullWidth size="lg" color="primary" className="rounded-[4px]">
         친구에게 공유하기
       </Button>
     </View>
