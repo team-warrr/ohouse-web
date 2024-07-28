b9df08976a02f1ef803d92ef1cbe23482952ac00
ghdtjgus76 <ghdtjgus76@naver.com>
2024-07-28 13:02:18 +0900

    chore: 행운 출첵 페이지 attendance로 이동

--- app/attendance/page.tsx
+++ app/attendance/page.tsx
@@ -1,3 +1,19 @@
+import { Button } from "@nextui-org/react";
+import Image from "next/image";
+
+import View from "@/shared/components/view";
+
 export default function AttendancePage() {
-  return <div>page</div>;
+  return (
+    <View>
+      <Image alt="attendance" src="/attendance-image.avif" width={500} height={500} />
+      <Button fullWidth size="lg" color="primary">
+        앱으로 입장하기 &gt;
+      </Button>
+      <Image alt="event-notice" src="/event-notice-image.avif" width={500} height={500} />
+      <Button fullWidth size="lg" color="primary">
+        친구에게 공유하기
+      </Button>
+    </View>
+  );
 }
