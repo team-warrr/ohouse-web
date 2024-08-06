55384bbe7f36d2b8ba7ff35f118913198e53592d
ShinYoung-Kim <lukey0515@naver.com>
2024-08-06 18:48:20 +0900

    design: 로그인 페이지 퍼블리싱

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,6 +3,7 @@
 import Logo from "@/shared/components/Logo";
 import View from "@/shared/components/view";
 import { signIn } from "next-auth/react";
+import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 
 const LoginPage = () => {
@@ -19,9 +20,21 @@ const LoginPage = () => {
 
   return (
     <View>
-      <Logo />
-      <button onClick={kakaoLogin}>카카오 로그인</button>
-      <button onClick={naverLogin}>네이버 로그인</button>
+      <div className="flex flex-col items-center justify-end">
+        <div className="flex flex-col items-center gap-10">
+          <Logo />
+          <div className="flex flex-col gap-2">
+            <button onClick={kakaoLogin}>
+              <Image alt="kakao" src="/kakao.png" width={319} height={40} />
+            </button>
+            <button onClick={naverLogin}>
+              <Image alt="naver" src="/naver.png" width={319} height={40} />
+            </button>
+          </div>
+          <h6 className="text-gray-400">로그인에 문제가 있으신가요?</h6>
+        </div>
+        <h6 className="fixed bottom-2 text-gray-500">비회원 주문 조회하기</h6>
+      </div>
     </View>
   );
 };
--- shared/components/view.tsx
+++ shared/components/view.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export default function View(props: PropsWithChildren) {
   return (
-    <main className="flex flex-col justify-center items-center gap-2 p-2 max-w-[700px] mx-auto">
+    <main className="flex flex-col justify-center items-center gap-2 p-2 max-w-[700px] mx-auto min-h-screen">
       {props.children}
     </main>
   );
