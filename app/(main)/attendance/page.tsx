52187438f62b67f4631c1a37d1eb504f37123bb0
ghdtjgus76 <ghdtjgus76@naver.com>
2024-09-07 01:13:09 +0900

    feat: 행운 출첵 페이지 앱으로 입장하기 딥링크 버튼 적용

--- app/(main)/attendance/page.tsx
+++ app/(main)/attendance/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@nextui-org/react";
 import Image from "next/image";
 
+import DeepLinkButton from "@/shared/components/deep-link-button";
 import View from "@/shared/components/view";
 
 const title = "행운 출첵";
@@ -16,9 +17,15 @@ export default function AttendancePage() {
   return (
     <View>
       <Image alt="attendance" src="/attendance-image.avif" width={500} height={500} />
-      <Button fullWidth size="lg" color="primary" className="rounded-[4px]">
+      <DeepLinkButton
+        fullWidth
+        size="lg"
+        color="primary"
+        className="rounded-[4px]"
+        path={"attendance"}
+      >
         앱으로 입장하기 &gt;
-      </Button>
+      </DeepLinkButton>
       <Image alt="event-notice" src="/event-notice-image.avif" width={500} height={500} />
       <Button fullWidth size="lg" color="primary" className="rounded-[4px]">
         친구에게 공유하기
