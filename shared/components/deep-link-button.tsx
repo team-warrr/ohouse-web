735dd18d91c3b7a389817e0083ca36ef4c59110e
ghdtjgus76 <ghdtjgus76@naver.com>
2024-09-07 01:12:52 +0900

    chore: 딥링크 버튼 next ui Button props 확장하도록 수정

--- shared/components/deep-link-button.tsx
+++ shared/components/deep-link-button.tsx
@@ -1,20 +1,22 @@
 "use client";
 
-import { Button } from "@nextui-org/react";
+import { Button, ButtonProps } from "@nextui-org/react";
 import { getEnvironment } from "@webviewkit/environment";
-import { CSSProperties, PropsWithChildren, useEffect, useState } from "react";
+import { CSSProperties, ReactNode, useEffect, useState } from "react";
 
 type DeepLinkButtonPropsType = {
   style?: CSSProperties;
   className?: string;
   path: string;
-} & PropsWithChildren;
+  children: ReactNode;
+} & ButtonProps;
 
 export default function DeepLinkButton({
   style,
   className,
   children,
   path,
+  ...props
 }: DeepLinkButtonPropsType) {
   const [showButton, setShowButton] = useState(false);
 
@@ -35,7 +37,7 @@ export default function DeepLinkButton({
 
   return (
     showButton && (
-      <Button style={style} className={className} onClick={handleClick}>
+      <Button style={style} className={className} onClick={handleClick} {...props}>
         {children}
       </Button>
     )
